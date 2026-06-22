7cdc5d31b37cb19b474b2fcce594a7f24cbab180
Trust in J <trustinjones7@gmail.com>
2026-06-22 09:59:30 -0700

    Initialized repository and added my first code.

---
+++ script.js
@@ -0,0 +1,21 @@
+function happy() {
+    document.getElementById("message").textContent = "Happy mode activated"; document.getElementById("page").style.background = "blue";
+}
+
+
+function sad() {
+    document.getElementById("message").textContent = "Sadness activated"; document.getElementById("page").style.background = "#111"
+}
+
+
+
+function jealous() {
+    document.getElementById("message").textContent = "Mood Detected Jealous"; document.getElementById("page").style.background = "purple";
+}
+
+
+function angry() {
+
+
+    
+}
